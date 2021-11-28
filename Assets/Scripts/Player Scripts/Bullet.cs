3a6815064f6f8ca5bf8f651097585013b0c6d6c3
Sercan Evyapan <sevyapan@gmail.com>
2021-11-28 17:45:56 +0300

    Add Props

---
+++ Assets/Scripts/Player Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Box")
+            Destroy(target.gameObject);
+
+    }
+}
