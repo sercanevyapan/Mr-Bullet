1901d574b1c32d8081361976fa67176537ddc5e9
Sercan Evyapan <sevyapan@gmail.com>
2021-11-28 19:31:41 +0300

    Add Tnt Prefab and Tnt.cs

---
+++ Assets/Scripts/Tnt.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tnt : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    public float radius = 1;
+    public float power = 10;
+
+
+    private void Explode()
+    {
+        Vector2 explosionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+
+        foreach (Collider2D hit in colliders)
+        {
+            if (hit.GetComponent<Rigidbody2D>() !=null)
+            {
+                var explodeDir = hit.GetComponent<Rigidbody2D>().position - explosionPos;
+
+                hit.GetComponent<Rigidbody2D>().gravityScale = 1;
+                hit.GetComponent<Rigidbody2D>().AddForce(power * explodeDir, ForceMode2D.Impulse);
+            }
+
+            if (hit.tag == "Enemy")
+                hit.tag = "Untagged";
+        }
+    }
+
+ 
+
+    private void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag =="Bullet")
+        {
+            GameObject exp = Instantiate(explosionPrefab);
+            exp.transform.position = transform.position;
+            Explode();
+            Destroy(exp, .8f);
+            Destroy(gameObject);
+        }
+    }
+
+   
+}
