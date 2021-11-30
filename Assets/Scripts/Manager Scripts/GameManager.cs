ce3b912fb2d7101377934361ebd88874c01893ac
Sercan Evyapan <sevyapan@gmail.com>
2021-11-30 15:43:37 +0300

    Add Levels

--- Assets/Scripts/Manager Scripts/GameManager.cs
+++ Assets/Scripts/Manager Scripts/GameManager.cs
@@ -74,4 +74,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
 }
